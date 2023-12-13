ab0611795ee7cc65510bac3d3a66dacd1c34c4e2
Edi0501 <edytanowakowska05@gmail.com>
2023-12-13 12:00:59 +0100

    Add project files.

---
+++ tydzien2zad5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace tydzień2zad5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello1!");
+            Console.WriteLine("Odpowiedz proszę na pytania");
+            Console.WriteLine("Imię:");
+            string imię;
+            imię = Console.ReadLine();
+
+            Console.WriteLine("Nazwisko:");
+            string nazwisko;
+            nazwisko = Console.ReadLine();
+
+            Console.WriteLine("Numer telefonu:");
+            string numerTelefonu;
+            numerTelefonu = Console.ReadLine();
+
+            Console.WriteLine("Adres email");
+            string AdresEmail;
+            AdresEmail = Console.ReadLine();
+
+            Console.WriteLine("Wzrost");
+            int wzrost;
+            wzrost = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Waga");
+            double waga;
+            waga = double.Parse(Console.ReadLine());
+
+        }
+    }
+}
